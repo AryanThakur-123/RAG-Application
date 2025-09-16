2e35cc2146930ce95728a0cdd12be7cb31f115a4
Aryan Thakur <23f1000966@ds.study.iitm.ac.in>
2025-09-16 23:36:12 +0530

    updated

--- ChatModels/wiki.py
+++ ChatModels/wiki.py
@@ -4,13 +4,16 @@ from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
 from langchain_google_genai import ChatGoogleGenerativeAI
 import warnings
 from dotenv import load_dotenv
+from  langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import Chroma
 
 warnings.filterwarnings("ignore")
 load_dotenv()
 
-# hf_token = os.getenv("HF_TOKEN")
-# if not hf_token:
-#     raise ValueError("Hugging Face API token not found.")
+hf_token = os.getenv("HF_TOKEN")
+if not hf_token:
+    raise ValueError("Hugging Face API token not found.")
 
 api_key = os.getenv("GOOGLE_API_KEY")
 
@@ -32,8 +35,6 @@ def fetch_wikipedia_summary(topic):
     wiki_wiki = wiki.Wikipedia(user_agent='23f1000966@ds.study.iitm.ac.in', language='en',extract_format=wiki.ExtractFormat.WIKI)
     page = wiki_wiki.page(topic)
     if page.exists():
-        # f = open("wiki.txt", "w", encoding="utf-8")
-        # f.write(topic)
         return ' '.join(page.text.split('. ')) + '.'
     else:
         return "Topic not found on Wikipedia."
@@ -41,9 +42,18 @@ def fetch_wikipedia_summary(topic):
 if __name__ == "__main__":
     topic = input("Enter the topic: ")
     topic = fetch_wikipedia_summary(topic)
-    print("Fetched Topic Content:", topic)
+    # print("Fetched Topic Content:", topic)
     f = open("wiki.txt", "w", encoding="utf-8")
     f.write(topic)
-    summary_response = model.invoke(topic)
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
+    chunks = text_splitter.split_text(topic)
+    # print('splitter=>', text_splitter.split_text(topic))
+    embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+    vec_store = Chroma.from_texts(chunks, embeddings,collection_name="wiki-collection")
+    result = vec_store.similarity_search(topic, k=3)
+    # print('result=>', result)
+    
+    docs_text = "\n\n".join([doc.page_content for doc in result])
+    summary_response = model.invoke(docs_text)
     print("Summary:", summary_response.content)
 
